6359626f9189e80b1b3517527a5e3f6481ff06bd
raj-kumar-pati <rajkumarpati6969@gmail.com>
2023-10-30 17:09:01 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.from("#text-div h1",{
+     x:-400,
+    duration:2,
+    stagger:1,
+    delay:2,
+    ease:Expo.easeOut
+})
+gsap.from("#verticalText>h1",{
+    x:"-100vh",
+    duration:3,
+    delay:2
+    
+})
+gsap.from("#part2_img",{
+    width:"0",
+    duration:3,
+    delay:2
+})
